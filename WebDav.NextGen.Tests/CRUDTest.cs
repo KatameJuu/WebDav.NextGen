8bb6d26ce415600e3ad8dc4372b89b8e597f9551
TheCheshireFox <ddrambler@gmail.com>
2021-03-10 04:56:01 +0300

    Add implementation, add tests.

---
+++ WebDav.NextGen.Tests/CRUDTest.cs
@@ -0,0 +1,115 @@
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Moq;
+using Moq.Language.Flow;
+using Moq.Protected;
+using WebDav.NextGen.Exceptions;
+using Xunit;
+
+namespace WebDav.NextGen.Tests
+{
+    public class CrudTest
+    {
+        static ISetup<HttpMessageHandler, Task<HttpResponseMessage>> SetupSendAsync(Mock<HttpMessageHandler> mock)
+        {
+            return mock
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>());
+        }
+        
+        static Mock<HttpMessageHandler> SetUp(HttpResponseMessage response)
+        {
+            var httpHandlerMock = new Mock<HttpMessageHandler>(MockBehavior.Strict);
+            
+            httpHandlerMock
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(response)
+                .Verifiable();
+            
+            httpHandlerMock
+                .Protected()
+                .Setup<HttpResponseMessage>("Send",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>())
+                .Returns(response)
+                .Verifiable();
+            
+            return httpHandlerMock;
+        }
+
+        static void VerifySendAsync(Mock<HttpMessageHandler> mock, Times times, Expression<Func<HttpRequestMessage, bool>> pred)
+        {
+            try
+            {
+                mock.Protected().Verify("SendAsync", times, ItExpr.Is(pred), ItExpr.IsAny<CancellationToken>());
+            }
+            catch (MockException)
+            {
+                mock.Protected().Verify("Send", times, ItExpr.Is(pred), ItExpr.IsAny<CancellationToken>());
+            }
+        }
+        
+        static void VerifySendAsync(Mock<HttpMessageHandler> mock, Expression<Func<HttpRequestMessage, bool>> pred)
+        {
+            VerifySendAsync(mock, Times.AtLeast(1), pred);
+        }
+        
+        [Fact]
+        public async Task CreateDirectorySuccessTest()
+        {
+            var response = new HttpResponseMessage(HttpStatusCode.Created);
+            
+            var hMock = new Mock<HttpMessageHandler>(MockBehavior.Strict);
+            SetupSendAsync(hMock).ReturnsAsync(response).Verifiable();
+            
+            var wd = new WebDav(new HttpClient(hMock.Object));
+            await wd.CreateDirectoryAsync(new Uri("https://foo.bar/baz"));
+
+            VerifySendAsync(hMock, req =>
+                req.Method == new HttpMethod("MKCOL") && req.RequestUri.ToString() == "https://foo.bar/baz");
+        }
+
+        [Theory]
+        [InlineData(HttpStatusCode.Unauthorized, typeof(AccessDenied))]
+        [InlineData(HttpStatusCode.Forbidden, typeof(AccessDenied))]
+        [InlineData(HttpStatusCode.Conflict, typeof(DoesNotExists))]
+        [InlineData(HttpStatusCode.InsufficientStorage, typeof(InsufficientSpace))]
+        public async Task CreateDirectoryThrowsSpecific(HttpStatusCode code, Type exceptionType)
+        {
+            var response = new HttpResponseMessage(code);
+            
+            var hMock = new Mock<HttpMessageHandler>(MockBehavior.Strict);
+            SetupSendAsync(hMock).ReturnsAsync(response).Verifiable();
+            
+            var wd = new WebDav(new HttpClient(hMock.Object));
+            await Assert.ThrowsAsync(exceptionType, () => wd.CreateDirectoryAsync(new Uri("https://foo.bar/baz")));
+        }
+
+        [Fact]
+        public async Task CreateDirectoryThrowsOnFailure()
+        {
+            foreach (var code in Enum.GetValues<HttpStatusCode>().Where(c => (int) c >= 400))
+            {
+                var response = new HttpResponseMessage(code);
+                
+                var hMock = new Mock<HttpMessageHandler>(MockBehavior.Strict);
+                SetupSendAsync(hMock).ReturnsAsync(response).Verifiable();
+                
+                var wd = new WebDav(new HttpClient(hMock.Object));
+
+                var ex = await Record.ExceptionAsync(() => wd.CreateDirectoryAsync(new Uri("https://foo.bar/baz")));
+                Assert.IsAssignableFrom<WebDavException>(ex);
+            }
+        }
+    }
+}
---
+++ WebDav.NextGen/WebDav.NextGen.cs
@@ -0,0 +1,121 @@
+using System.Threading;
+using System.IO;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WebDav.NextGen.Exceptions;
+
+namespace WebDav.NextGen
+{
+    public class WebDav
+    {
+        private HttpClient _client;
+
+        public WebDav()
+        {
+            _client = new HttpClient();
+        }
+
+        public WebDav(HttpClient client)
+        {
+            _client = client;
+        }
+        
+        public async Task CreateDirectoryAsync(Uri path, CancellationToken token = default)
+        {
+            var method = new HttpMethod("MKCOL");
+            var req = new HttpRequestMessage(method, path);
+
+            var resp = await _client.SendAsync(req, token);
+
+            ThrowOnForbidden(resp);
+
+            if (!resp.IsSuccessStatusCode)
+            {
+                switch (resp.StatusCode)
+                {
+                    case HttpStatusCode.MethodNotAllowed:
+                        throw new AlreadyExists(resp.RequestMessage?.RequestUri?.ToString());
+                    case HttpStatusCode.Conflict:
+                        throw new DoesNotExists(resp.RequestMessage?.RequestUri?.ToString());
+                    case HttpStatusCode.InsufficientStorage:
+                        throw new InsufficientSpace(resp.RequestMessage?.RequestUri?.ToString());
+                    default:
+                        throw new WebDavException(resp.Content == null ? "" : await resp.Content.ReadAsStringAsync(), resp.StatusCode);
+                }
+            }
+        }
+
+        public Task<string> GetStringAsync(Uri path, CancellationToken token = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<byte[]> GetBytesAsync(Uri path, CancellationToken token = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Stream> GetStreamAsync(Uri path, CancellationToken token = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task WriteFileAsync(Uri path, byte[] data, CancellationToken token = default)
+        {
+            return WriteFileInternal(path, new ByteArrayContent(data), token);
+        }
+
+        public async Task WriteFileAsync(Uri path, Stream stream, bool takeOwnership = false, CancellationToken token = default)
+        {
+            await WriteFileInternal(path, new StreamContent(stream), token);
+
+            if (takeOwnership)
+            {
+                await stream.DisposeAsync();
+            }
+        }
+
+        public Task DeleteFileAsync(Uri path, CancellationToken token = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void ThrowOnForbidden(HttpResponseMessage resp)
+        {
+            if (!resp.IsSuccessStatusCode)
+            {
+                switch (resp.StatusCode)
+                {
+                    case HttpStatusCode.Forbidden:
+                    case HttpStatusCode.Unauthorized:
+                        throw new AccessDenied(resp.RequestMessage?.RequestUri?.ToString());
+                    default:
+                        return;
+                }
+            }
+        }
+
+        private async Task WriteFileInternal(Uri path, HttpContent content, CancellationToken token = default)
+        {
+            var req = new HttpRequestMessage(HttpMethod.Put, path)
+            {
+                Content = content
+            };
+
+            var resp = await _client.SendAsync(req, token);
+
+            if (!resp.IsSuccessStatusCode)
+            {
+                switch (resp.StatusCode)
+                {
+                    case HttpStatusCode.Conflict:
+                        throw new DoesNotExists(resp.RequestMessage?.RequestUri?.ToString());
+                    default:
+                        throw new WebDavException(resp.Content == null ? "" : await resp.Content.ReadAsStringAsync(), resp.StatusCode);
+                }
+            }
+        }
+    }
+}
